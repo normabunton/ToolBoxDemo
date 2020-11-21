34fe190443243e09b4892702cb27a9522ea203ad
normabunton <normajbunton@gmail.com>
2020-11-21 08:54:14 -0500

    firstconsoleapp

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBoxDemo
+{
+    class Program
+    {
+        //The main method acts as our programs' .exe file aka executable or entry point this is the file we double click on to sart
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to your Toolbox");
+            Console.WriteLine("Hit enter to clear the console and continue on with the code");
+            Console.ReadLine();
+
+
+            Console.Clear();
+            Console.WriteLine("Congratulations youve cleared the console.... hit enter to close the program");
+            Console.ReadLine();
+        }
+    }
+}
